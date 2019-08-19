00d8400f4cdba830abedfa1dfe28e038d607e530
liuhuiyu <328609918@qq.com>
2019-08-19 11:18:17 +0800

    初始化项目

--- src/components/element.vue
+++ src/components/element.vue
@@ -1,12 +1,11 @@
 <template>
-  <div id="app">
-
+  <div>
   </div>
 </template>
 
 <script>
-
 export default {
+
 }
 </script>
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.less'
+Vue.use(element)
 Vue.config.productionTip = false
 
 new Vue({
