daf44cd0e1759a720a2af9f926eb2336364bda4c
liuhuiyu <328609918@qq.com>
2019-08-20 12:08:23 +0800

    home大致布局

---
+++ src/components/home/home-left.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div class="title">
+      <img src="../../assets/imgs/logo_admin.png" alt="">
+    </div>
+
+</div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang='less' scoped>
+    .title{
+        padding: 16px 0 ;
+        text-align: center;
+        background-color: #2e2f32;
+        img{
+            width: 120px;
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,31 @@
 <template>
 <div>
-    主页面
+    <el-container>
+        <el-aside class="homeLeft" width="200px">
+            <home-left></home-left>
+        </el-aside>
+        <el-container>
+            <el-header>
+                头部
+            </el-header>
+            <el-main>
+                主要内容
+            </el-main>
+        </el-container>
+    </el-container>
 </div>
 </template>
 <script>
+import homeLeft from '../../components/home/home-left.vue'
 export default {
+  components: {
+    'home-left': homeLeft
+  }
 }
 </script>
-<style>
+<style lang="less" scoped>
+    .homeLeft{
+        height: 100vh;
+        background-color: #323745;
+    }
 </style>
